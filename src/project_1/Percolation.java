96a8b5a741bb5f9db9c0c3c2715f98104a85a96d
Xingmin Zhang <kingmanzhang@gmail.com>
2017-03-25 20:44:12 -0500

    Percolation is working

--- src/project_1/Percolation.java
+++ src/project_1/Percolation.java
@@ -1,99 +1,82 @@
 package project_1;
 
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
 public class Percolation {
 	
-	private int n;
+	private int N;
 	private boolean[] status;
-	private int[] id;
-	private int[] sz;
+	private WeightedQuickUnionUF grid;
 	private int openCount;
 	
    public Percolation(int n)  {// create n-by-n grid, with all sites blocked
    	if (n <= 0) 
    		throw new IllegalArgumentException();
-   	this.n = n;
+   	this.N = n;
    	this.openCount = 0;
-   	this.id = new int[n * n + 1];
-   	this.sz = new int[n * n + 1];
-   	this.status= new boolean[n * n + 1];
-   	for (int i = 0; i < n * n + 2; i++) {
-   		id[i] = i;
-   		sz[i] = 1;
+   	this.grid = new WeightedQuickUnionUF(n * n + 2);
+   	this.status= new boolean[n * n + 2];
+   	for (int i = 1; i < n * n + 1; i++) {
    		status[i] = false;
    	}
-   	status[0] = true;
-   	status[n * n + 1] = true;
+   	status[0] = true;  //first site reserved for top virtual site
+   	status[n * n + 1] = true; //last site reserved for bottom virtual site
    
    }
    
-   private int root(int i) {
-		while(i != id[i]) {
-			id[i] = id[id[i]];
-			i = id[i];
-		}
-		return i;
-	}
-	
-	private boolean connected(int p, int q) {
-		return root(p) == root(q);
-	}
-	
-	private void union(int p, int q) {
-		int i = root(p);
-		int j = root(q);
-		if(i == j) return;
-		if (sz[i] < sz[j]) {
-			id[i] = j;
-			sz[j] += sz[i];
-		} else {
-			id[j] = i;
-			sz[i] += sz[j];
-		}	
-	}
    
    public void open(int row, int col) {    // open site (row, col) if it is not open already
    
-   	if (row > n || row < 1 || col > n || col < 1)
+   	if (row > N || row < 1 || col > N || col < 1)
    		throw new IndexOutOfBoundsException();
    	
    	if (!isOpen(row, col)) {
-   		status[row * (row - 1) + col] = true ;
-   		if(col + 1 < n && isOpen(row, col + 1)) {
-   			union((row - 1) * n + col, (row - 1) * n + col + 1);
-   		} 
-   		if(col > 1 && isOpen(row, col -1)) {
-   			union ((row - 1) * n + col, (row -1) * n + col - 1);
-   		}
-   		if(row + 1 < n && isOpen(row + 1, col)) {
-   			union((row - 1) * n + col, row * n + col);
-   		}
-   		if(row > 1 && isOpen(row - 1, col)) {
-   			union((row - 1) * n + col, (row - 2) * n + col);
-   		}
-   		if(row == 1) {
-   			union((row - 1) * n + col, 0);
-   		}
-   		if(row == n) {
-   			union((row - 1) * n + col, n * n + 1);
-   		}
-   		openCount++;
-   	}
    		
-   	
-   	
+   		status[(row - 1) * N + col] = true ; //change status
+   		openCount++; //update counts of open site
+   		//union with its open neighbors 
+   		//first deal with its right neighbor 
+   		if(col < N && isOpen(row, col + 1)) {
+   			grid.union((row - 1) * N + col, (row - 1) * N + col + 1);
+   		}
+   		
+   		//deal with left neighbor
+   		if(col > 1 && isOpen(row, col -1)) {
+   			grid.union ((row - 1) * N + col, (row -1) * N + col - 1);
+   		}
+   		
+   		//deal with bottom neighbor
+   		if(row < N && isOpen(row + 1, col)) {
+   			grid.union((row - 1) * N + col, row * N + col);
+   		}
+   		
+   		//deal with upper bottom
+   		if(row > 1 && isOpen(row - 1, col)) {
+   			grid.union((row - 1) * N + col, (row - 2) * N + col);
+   		}
+   		
+   		//deal with special case
+   		//if open a site in the first row, it should union with virtual point
+   		if(row == 1) {
+   			grid.union(col, 0);
+   		}
+   		if(row == N) {
+   			grid.union((row - 1) * N + col, N * N + 1);
+   		}  		
+   	} 	
    }
    public boolean isOpen(int row, int col) { // is site (row, col) open?
-   	if (row > n || row < 1 || col > n || col < 1)
+   	if (row > N || row < 1 || col > N || col < 1)
    		throw new IndexOutOfBoundsException();
    		
-   	return status[(row - 1) * n + col];
+   	return status[(row - 1) * N + col];
    }	
    
    public boolean isFull(int row, int col) { // is site (row, col) full?
-   	if (row > n || row < 1 || col > n || col < 1)
+   	if (row > N || row < 1 || col > N || col < 1)
    		throw new IndexOutOfBoundsException();
    	 	
-   	return connected(row * (n - 1) + col, 0);
+   	return grid.connected((row - 1) * N + col, 0);
    
    }
    public int numberOfOpenSites() {// number of open sites
@@ -103,13 +86,22 @@ public class Percolation {
    }
    public boolean percolates() {// does the system percolate?
    
-   	return connected(0, n * n + 1);
+   	return grid.connected(0, N * N + 1);
    
    }
    
  
    public static void main(String[] args) {
-   	// test client (optional)
+   	
+   	Percolation test = new Percolation(4);
+   	System.out.println("Percolation? " + test.percolates());
+   	test.open(1, 2);
+   	System.out.println("Percolation? " + test.percolates());
+   	test.open(2, 2);
+   	test.open(4, 2);
+   	System.out.println("Percolation? " + test.percolates());
+   	test.open(3, 2);
+   	System.out.println("Percolation? " + test.percolates());
    }
 }
 
